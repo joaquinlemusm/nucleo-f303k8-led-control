e543d751eb697f4ae6a4398093e8af88f806cbc9
Aníbal Joaquín Lemus Martínez <91865286+joaquinlemusm@users.noreply.github.com>
2025-03-31 00:11:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -7,29 +7,29 @@
 int main(void) {
     set_up();
     while (1) {
-        if (GPIOA->IDR & MASK(0)) {     // PA0 = 1 (bot髇 presionado, conectado a VCC)
+        if (GPIOA->IDR & MASK(0)) {     // PA0 = 1 (bot贸n presionado, conectado a VCC)
             GPIOB->ODR |= MASK(0);      // Encender LED en PB0
-        } else {                        // PA0 = 0 (bot髇 no presionado, pull-down)
+        } else {                        // PA0 = 0 (bot贸n no presionado, pull-down)
             GPIOB->ODR &= ~MASK(0);     // Apagar LED en PB0
         }
-				if (GPIOA->IDR & MASK(1)) {     // PA1 = 1 (bot髇 presionado, conectado a VCC)
+	if (GPIOA->IDR & MASK(1)) {     // PA1 = 1 (bot贸n presionado, conectado a VCC)
             GPIOB->ODR |= MASK(7);      // Encender LED en PB0
-        } else {                        // PA1 = 0 (bot髇 no presionado, pull-down)
+        } else {                        // PA1 = 0 (bot贸n no presionado, pull-down)
             GPIOB->ODR &= ~MASK(7);     // Apagar LED en PB7
         }
-				if (GPIOA->IDR & MASK(3)) {     // PA3 = 1 (bot髇 presionado, conectado a VCC)
+	if (GPIOA->IDR & MASK(3)) {     // PA3 = 1 (bot贸n presionado, conectado a VCC)
             GPIOB->ODR |= MASK(6);      // Encender LED en PB0
-        } else {                        // PA3 = 0 (bot髇 no presionado, pull-down)
+        } else {                        // PA3 = 0 (bot贸n no presionado, pull-down)
             GPIOB->ODR &= ~MASK(6);     // Apagar LED en PB6
         }
-				if (GPIOA->IDR & MASK(4)) {     // PA4 = 1 (bot髇 presionado, conectado a VCC)
+	if (GPIOA->IDR & MASK(4)) {     // PA4 = 1 (bot贸n presionado, conectado a VCC)
             GPIOB->ODR |= MASK(1);      // Encender LED en PB0
-        } else {                        // PA1 = 0 (bot髇 no presionado, pull-down)
+        } else {                        // PA1 = 0 (bot贸n no presionado, pull-down)
             GPIOB->ODR &= ~MASK(1);     // Apagar LED en PB1
         }
-				if (GPIOA->IDR & MASK(7)) {     // PA7 = 1 (bot髇 presionado, conectado a VCC)
+	if (GPIOA->IDR & MASK(7)) {     // PA7 = 1 (bot贸n presionado, conectado a VCC)
             GPIOB->ODR |= MASK(5);      // Encender LED en PB0
-        } else {                        // PA7 = 0 (bot髇 no presionado, pull-down)
+        } else {                        // PA7 = 0 (bot贸n no presionado, pull-down)
             GPIOB->ODR &= ~MASK(5);     // Apagar LED en PB5
         }
     }
